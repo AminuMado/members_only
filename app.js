c8d8a83b9a342a7d09bf3e519f8f7c5a5ce6253b
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-09-07 12:15:34 +0100

    add passport initialization

--- app.js
+++ app.js
@@ -6,9 +6,94 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-
 var app = express();
 
+/**
+ * -------------- DATABASE ----------------
+ */
+
+/**
+ * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place the following
+ * string into the `.env` file
+ *
+ * MONGODB_URI=mongodb://<user>:<password>@localhost:27017/database_name
+ */
+const mongoose = require("mongoose");
+const dbString = process.env.MONGODB_URI;
+const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(dbString, dbOptions);
+const db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+/**
+ * -------------- SESSION SETUP ----------------
+ */
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const sessionStore = new MongoStore({
+  mongoUrl: dbString,
+  collectionName: "sessions",
+});
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+    },
+  })
+);
+
+/**
+ * -------------- PASSPORT AUTHENTICATION ----------------
+ */
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const User = require("./models/user");
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) return done(err);
+      if (!user) return done(null, false, { message: "Incorrect username" });
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) return done(err);
+        // Passwords match, log user in!
+        if (res) return done(null, user);
+        // Passwords do not match!
+        else return done(null, false, { message: "Incorrect password" });
+      });
+    });
+  })
+);
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) =>
+  User.findById(id, (err, user) => done(err, user))
+);
+
+// Secret value should be a process env value
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+// Access the user object from anywhere in our application
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  console.log(req.user);
+  next();
+});
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
