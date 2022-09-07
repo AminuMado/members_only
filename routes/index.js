370a2b9c9841b935591c60e545282fb548264c17
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-09-07 12:16:21 +0100

    add logout route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
-
+const authController = require("../controllers/authController");
+const messageController = require("../controllers/messageController");
+const profileController = require("../controllers/profileController");
 /**
  * -------------- POST ROUTES ----------------
  */
+router.post("/signup", authController.signup_post);
+router.post("/login", authController.login_post);
+router.post("/create-message", messageController.create_message_post);
+router.post("/profile", profileController.profile_post);
 
 /**
  * -------------- GET ROUTES ----------------
@@ -15,23 +21,18 @@ router.get("/", function (req, res, next) {
 });
 
 /* Sign Up Page. */
-router.get("/signup", function (req, res, next) {
-  res.render("signup_form", { title: "Sign Up" });
-});
+router.get("/signup", authController.signup_get);
 
 /* Log In Page. */
-router.get("/login", function (req, res, next) {
-  res.render("login_form", { title: "Login" });
-});
+router.get("/login", authController.login_get);
 
 /* Profile Page. */
-router.get("/profile", function (req, res, next) {
-  res.render("profile", { title: "Profile" });
-});
+router.get("/profile", profileController.profile_get);
 
 /* Create Message Page. */
-router.get("/create-message", function (req, res, next) {
-  res.render("create_message_form", { title: "Create Message" });
-});
+router.get("/create-message", messageController.create_message_get);
+
+/* Log Out. */
+router.get("/logout", authController.logout_get);
 
 module.exports = router;
